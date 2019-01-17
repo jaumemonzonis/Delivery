7f6031054851cbd603b7510661687b6678ca9164
a045795512m <a045795512m@s09-pc22.ausias.lan>
2019-01-17 20:36:11 +0100

    Restaurante crud

---
+++ src/main/webapp/js/app/restaurante/edit.js
@@ -0,0 +1,78 @@
+"use strict";
+
+moduleRestaurante.controller("restauranteEditController", [
+    "$scope",
+    "$http",
+    "$routeParams",
+    "toolService",
+    'sessionService',
+    '$window',
+    function ($scope, $http, $routeParams, toolService, sessionService,$window) {
+
+        $scope.visualizar = false;
+        $scope.logged = false;
+
+        if (!$routeParams.id) {
+            $scope.id = 1;
+        } else {
+            $scope.id = $routeParams.id;
+        }
+
+        $scope.mostrar = false;
+        $scope.activar = true;
+        $scope.ajaxData = "";
+
+   
+
+        $http({
+            method: "GET",
+            url: 'json?ob=restaurante&op=get&id=' + $scope.id
+        }).then(function (response) {
+            console.log(response);
+
+            $scope.id = response.data.message.id;
+            $scope.nombre = response.data.message.nombre;
+            $scope.direccion = response.data.message.direccion;
+            $scope.poblacion = response.data.message.poblacion;
+
+
+        }), function (response) {
+            console.log(response);
+        };
+
+        $scope.isActive = toolService.isActive;
+
+        $scope.update = function () {
+
+            var json = {
+                id: $scope.id,
+                nombre: $scope.nombre,
+                direccion: $scope.direccion,
+                poblacion: $scope.poblacion
+
+            }
+
+            $http({
+                method: 'GET',
+                header: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                url: 'json?ob=restaurante&op=update',
+                params: {json: JSON.stringify(json)}
+            }).then(function () {
+                  $scope.visualizar = true;
+            })
+        }
+
+
+          $scope.volver = function () {
+            $window.history.back();
+        }
+        $scope.close = function () {
+            $location.path('/home');
+        };
+        $scope.plist = function () {
+            $location.path('/' + $scope.ob + '/plist');
+        };
+
+    }]);
