2fab3c9cab5857531db4d6c96860ebab70873130
jaume monzonis <jaume monzonis@158.42.191.93>
2019-02-04 14:38:21 +0100

    Plist producto carrito inicio

--- src/main/webapp/js/app/carrito/plist.js
+++ src/main/webapp/js/app/carrito/plist.js
@@ -204,8 +204,16 @@ moduleCarrito.controller('carritoPlistController', ['$scope', '$http', '$locatio
 
         }
 
-
+	$('#product-card').hover(function(){
+			$(this).addClass('animate');
+			$('div.carouselNext, div.carouselPrev').addClass('visible');			
+		 }, function(){
+			$(this).removeClass('animate');			
+			$('div.carouselNext, div.carouselPrev').removeClass('visible');
+	});	
 
     }
+    
+    
 
 ]);
