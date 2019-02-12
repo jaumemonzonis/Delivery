9342a77b1c0bcd3cda5c9941d3cd50836aac945a
a045795512m <a045795512m@s09-pc22.ausias.lan>
2019-02-12 17:27:12 +0100

    Alerta no logeado a login

--- src/main/webapp/js/app/common/home.js
+++ src/main/webapp/js/app/common/home.js
@@ -2,14 +2,18 @@
 
 moduleCommon.controller('homeController', ['$scope', '$location', 'toolService', 'sessionService',
     function ($scope, $location, toolService, sessionService) {
-        $scope.logged = false;
+
         $scope.ruta = $location.path();
         $scope.isActive = toolService.isActive;
-//
-//   if (sessionService.getUserName() !== "") {
-//            $scope.loggeduser = sessionService.getUserName();
-//            $scope.loggeduserid = sessionService.getId();
-//            $scope.logged = true;
-//            $scope.tipousuarioID = sessionService.getTypeUserID();
-//        }
+         
+         var logged= sessionService.isSessionActive();
+
+       $scope.productos = function () {
+           if (logged === true){
+            $location.url(`carrito/plist_burger`);
+        } else {
+            $location.url(`usuario/login`);
+        }
+        };
+
     }]);
